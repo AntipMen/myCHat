66e22be3ae1be0ccb85303ccdd194b5bb2e39732
AnnaAntipova <Anna.Antipova794@gmail.com>
2020-05-29 18:47:06 +0300

    Updates

---
+++ precache-manifest.e699d7544e55d6e1c33215fd6748ff7a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a190f5b09b88a48e0f4c56bb892adc14",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9912ccd309843bf3caf2",
+    "url": "./static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "1208261b79da2cfcb4b1",
+    "url": "./static/css/main.92112109.chunk.css"
+  },
+  {
+    "revision": "9912ccd309843bf3caf2",
+    "url": "./static/js/2.1f3293fb.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "./static/js/2.1f3293fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1208261b79da2cfcb4b1",
+    "url": "./static/js/main.16f97092.chunk.js"
+  },
+  {
+    "revision": "ca83f5861ad62afb5b2d",
+    "url": "./static/js/runtime-main.d4aa9273.js"
+  },
+  {
+    "revision": "80bf226b965774bb4aa21bda8c02202e",
+    "url": "./static/media/alien.80bf226b.svg"
+  },
+  {
+    "revision": "2ded17b8ae32209fe538a805f3408974",
+    "url": "./static/media/boyDog.2ded17b8.svg"
+  },
+  {
+    "revision": "5a31b6a3f7b23c64990c29fc048d21a8",
+    "url": "./static/media/fly.5a31b6a3.svg"
+  }
+]);
